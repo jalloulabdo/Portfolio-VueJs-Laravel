cbf9d00d5f97f657e81e731a447d7b755f6bc684
Abderrahim JALLOUL <jalloul@greensolutionsmaroc.com>
2023-02-07 16:03:27 +0100

    vit config   commit

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,14 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        vue(),
+        vue({
+            // This is needed, or else Vite will try to find image paths (which it wont be able to find because this will be called on the web, not directly)
+            // For example <img src="/images/logo.png"> will not work without the code below
+            template: {
+                transformAssetUrls: {
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
 });
